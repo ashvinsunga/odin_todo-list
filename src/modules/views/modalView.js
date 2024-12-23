3e5bccea16cdd2816f00f2db5358eb33ec37f282
Ashvin <ashvinsunga@yahoo.com>
2024-12-23 16:57:59 +0800

    Added modal blur and unblur

--- src/modules/views/modalView.js
+++ src/modules/views/modalView.js
@@ -1,5 +1,9 @@
-export function addBlur() {
+function addBlur() {
     const container = document.querySelector('[container]');
     container.classList.add('blurred-background');
   }
 
+function removeBlur() {
+    const mainContent = document.querySelector('[container]');
+    mainContent.classList.remove('blurred-background');
+  }
